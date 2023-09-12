48516bcf882e33bf27e4552f5b7cfa10a30ca7ad
davidxiong <daweix3@illinois.edu>
2023-09-12 02:41:41 -0500

    udpatre 1

--- postlab1.py
+++ postlab1.py
@@ -13,22 +13,22 @@ sense.set_pixel(x, y,color)
 
 while True:
     for event in sense.stick.get_events():
-        if event.action == "pressed":
+        if event.action == "pressed" and event.direction == "middle":
             sense.clear()
             break
-        elif event.direction == "up":
+        elif event.action == "pressed" and event.direction == "up":
             sense.clear()
             for i in range(0, y):
                 sense.set_pixel(x, i, color)
-        elif event.direction == "down":
+        elif event.action == "pressed" and event.direction == "down":
             sense.clear()
             for i in range(y, 8):
                 sense.set_pixel(x, i, color)
-        elif event.direction == "left":
+        elif event.action == "pressed" and event.direction == "left":
             sense.clear()
             for i in range(0, x):
                 sense.set_pixel(i, y, color)
-        elif event.direction == "right":
+        elif event.action == "pressed" and event.direction == "right":
             sense.clear()
             for i in range(x, 8):
                 sense.set_pixel(i, y, color)
---
+++ postlab2.py
@@ -0,0 +1,6 @@
+from sense import SenseHat
+from time import sleep
+
+sense = SenseHat()
+sense.clear()
+
