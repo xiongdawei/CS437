a5639f8e984d7560691307e0b2e00fd53d8bbe09
davidxiong <daweix3@illinois.edu>
2023-09-12 02:03:09 -0500

    change all

--- postlab1.py
+++ postlab1.py
@@ -9,7 +9,7 @@ color = (255,0,0)
 
 x = 3
 y = 5
-sense.show_pixel(x, y,color)
+sense.set_pixel(x, y,color)
 
 while True:
     for event in sense.stick.get_events():
@@ -19,20 +19,20 @@ while True:
         if event.direction == "up":
             sense.clear()
             for i in range(0, y):
-                sense.show_pixel(x, i, color)
+                sense.set_pixel(x, i, color)
             sense.clear(color)
         elif event.direction == "down":
             sense.clear()
             for i in range(y, 8):
-                sense.show_pixel(x, i, color)
+                sense.set_pixel(x, i, color)
         elif event.direction == "left":
             sense.clear()
             for i in range(0, x):
-                sense.show_pixel(i, y, color)
+                sense.set_pixel(i, y, color)
         elif event.direction == "right":
             sense.clear()
             for i in range(x, 8):
-                sense.show_pixel(i, y, color)
+                sense.set_pixel(i, y, color)
         else:
             pass
 
